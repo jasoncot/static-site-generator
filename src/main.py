122fb30eb8debc1b3a9399504655db55a41a7271
jasoncot <jasoncot@gmail.com>
2025-01-13 08:04:39 -0800

    Ch2.L2 - adding unit tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-	text_node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+	text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
 	print(text_node)
 
 main()
