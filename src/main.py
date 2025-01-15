e4f4f6e9628ff939b07a0eb39f5aac4a3698430a
jasoncot <jasoncot@gmail.com>
2025-01-14 20:29:14 -0800

    Ch5L1 - Updated main to copy static files

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import os
 import shutil
 from textnode import TextNode, TextType
 
-def files_and_dirs_accumulator(files_and_dirs, item):
-	if os.path.is_file(item):
-		return [files_and_dirs[0] + [item], files_and_dirs[1]]
-	return [files_and_dirs[0], files_and_dirs[1] + [item]]
+def set_source_dir_for_accumulator(dir):
+	def files_and_dirs_accumulator(files_and_dirs, item):
+		if os.path.isfile(os.path.join(dir, item)) == True:
+			return [files_and_dirs[0] + [item], files_and_dirs[1]]
+		return [files_and_dirs[0], files_and_dirs[1] + [item]]
+	
+	return files_and_dirs_accumulator
 
 def copy_dir_files_to_dest(src_path, dest_path):
 	# make sure we have files that we will be doing things
@@ -14,13 +17,16 @@ def copy_dir_files_to_dest(src_path, dest_path):
 		raise Exception("source path does not exist")
 	
 	if os.path.exists(dest_path):
+		print(f"Cleaning up new destination path ({dest_path})")
 		shutil.rmtree(dest_path)
 
-	os.path.mkdir(dest_path)
-	
+	print(f"Creating destination directory ({dest_path})")
+	os.mkdir(dest_path)
+
 	[files, dirs] = functools.reduce(
-		files_and_dirs_accumulator,
-		os.listdir(src_path)
+		set_source_dir_for_accumulator(src_path),
+		os.listdir(src_path),
+		[[], []]
 	)
 
 	print(f"Copying files from directory {src_path} -> {dest_path}")
@@ -29,16 +35,13 @@ def copy_dir_files_to_dest(src_path, dest_path):
 		shutil.copy(os.path.join(src_path, file), os.path.join(dest_path, file))
 
 	for dir in dirs:
-		print(f"Moving into subdirectory {src_path} {dir}")
+		print(f"Moving into subdirectory {os.path.join(src_path, dir)}")
 		copy_dir_files_to_dest(
 			os.path.join(src_path, dir),
 			os.path.join(dest_path, dir)
 		)
 
-
-
 def main():
-	text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-	print(text_node)
+	copy_dir_files_to_dest('./static', './public')
 
 main()
