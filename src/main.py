a52a550eb707d13c2666b5eb8006d2d991926d63
jasoncot <jasoncot@gmail.com>
2025-01-14 21:41:20 -0800

    Ch5.L4 - added majesty file

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from textnode import TextNode, TextType
 from utils import generate_page
+import re
 
 def set_source_dir_for_accumulator(dir):
 	def files_and_dirs_accumulator(files_and_dirs, item):
@@ -42,8 +43,42 @@ def copy_dir_files_to_dest(src_path, dest_path):
 			os.path.join(dest_path, dir)
 		)
 
+def crawl_dir_for_files(src_path):
+	if not os.path.exists(src_path):
+		raise Exception("source path does not exist")
+
+	[files, dirs] = functools.reduce(
+		set_source_dir_for_accumulator(src_path),
+		os.listdir(src_path),
+		[[], []]
+	)
+
+	if len(files) == 0 and len(dirs) == 0:
+		return []
+	
+	collected_files = []
+	collected_files += list(map(lambda file: os.path.join(src_path, file), files))
+	collected_files += functools.reduce(
+		lambda acc, dir: acc + crawl_dir_for_files(os.path.join(src_path, dir)),
+		dirs,
+		[]
+	)
+	return collected_files
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+	files_to_convert = crawl_dir_for_files(dir_path_content)
+	print("files to convert: ", files_to_convert)
+	pairs = list(map(
+		lambda file_name: [file_name, re.sub(r"\.md$", ".html", file_name).replace(dir_path_content, dest_dir_path)],
+		files_to_convert
+	))
+	for pair in pairs:
+		generate_page(pair[0], template_path, pair[1])
+
+
 def main():
 	copy_dir_files_to_dest('./static', './public')
-	generate_page("./content/index.md", "./src/template.html", "./public/index.html")
+	generate_pages_recursive("./content", "./src/template.html", "./public")
+
 
 main()
